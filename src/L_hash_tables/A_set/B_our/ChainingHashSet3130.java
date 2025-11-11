a5e70ae44c810f2703c464b9ea176b465f7fc1ee
Laura Williams <107360787+laura-williams-1@users.noreply.github.com>
2025-11-11 15:25:46 -0500

    Class notes

--- src/L_hash_tables/A_set/B_our/ChainingHashSet3130.java
+++ src/L_hash_tables/A_set/B_our/ChainingHashSet3130.java
@@ -37,15 +37,19 @@ public class ChainingHashSet3130<E> implements Set3130<E> {
         return size;
     }
 
-    @Override
+    @Overrides
     public boolean add(E element) {
         if (contains(element)) {
+            // in the element is already an element return false 
+            // this is the contains methods we created for Hashset 
             return false;
         }
 
         int index = hash(element);
+        //create an index for this 
 
         if (table[index] == null) {
+            // if there is no linked list, create the linked list at the index of the table 
             table[index] = new LinkedList<>();
         }
 
@@ -53,6 +57,7 @@ public class ChainingHashSet3130<E> implements Set3130<E> {
         size++;
 
         if (loadFactor() > maxLoadFactor) {
+            //Resize and grow 
             grow();
         }
 
@@ -64,12 +69,18 @@ public class ChainingHashSet3130<E> implements Set3130<E> {
         int index = hash(o);
 
         if (table[index] == null) {
+            // if the index we are looking for is null there are no linked lists return false 
             return false;
         } else if (!table[index].contains(o)) {
+            // if there is a linked list at this bucket but o is not in the LL return false 
             return false;
         } else {
             table[index].remove(o);
+            // table at this index is a Containter a linekd list 
+            // in the case where we find the o we remove it. 
+            // whats the remove method being called here
             size--;
+            // after removing decrease the size 
             return true;
         }
     }
@@ -78,6 +89,10 @@ public class ChainingHashSet3130<E> implements Set3130<E> {
     public boolean contains(Object o) {
         int index = hash(o);
         return table[index] != null && table[index].contains(o);
+        // first thing to do is check if there is the index of the hashcode is null. then chek if 
+        // how does the conaitns methods runs in a cosntatn time becausse the length of the LL of your hashset is shoty 
+
+        // why does the contains method runi na constant time vs not O(n) when in a hashset 
     }
 
     @Override
@@ -104,6 +119,8 @@ public class ChainingHashSet3130<E> implements Set3130<E> {
     // given any object, returns the index at which the object belongs
     private int hash(Object o) {
         return Math.abs(o.hashCode()) % table.length;
+        // hashcode might be a negaatiave thts why we call abs value 
+        
     }
 
     // returns the current load factor
@@ -113,14 +130,20 @@ public class ChainingHashSet3130<E> implements Set3130<E> {
 
     @SuppressWarnings("unchecked")
     private void grow() {
+        // keep a ref to the old table 
         LinkedList<E>[] oldTable = table;
+        // table is now pointed to a new array full of linked lsit that's twice the length plus one
         table = (LinkedList<E>[]) new LinkedList[2 * table.length + 1];
+        // set size to zero, why?
+        // because the add method will automatically increase the size 
         size = 0;
-
+// old buck is a linked list 
         for (LinkedList<E> oldBucket : oldTable) {
             if (oldBucket != null) {
+                // whats going on in the for loop? were adding the old linked list ?
                 for (E element : oldBucket) {
                     add(element);
+                    // adds the method to the table and also increases the size 
                 }
             }
         }
