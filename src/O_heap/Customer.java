b2f4d243ea7d75d3ba78bb4fc09286b6be027140
molach <lach@sci.brooklyn.cuny.edu>
2025-12-02 13:54:58 -0500

    heap

--- src/O_heap/Customer.java
+++ src/O_heap/Customer.java
@@ -1,14 +1,14 @@
 package O_heap;
 
-public record Customer(String name, int importance) implements Comparable<Customer> {
+public record Customer(String name, int years) implements Comparable<Customer> {
     @Override
     public String toString() {
-        return name + " " + importance;
+        return name + " " + years;
     }
 
-    // compares customers based on importance level
+    // compares customers based on how long they were customers
     @Override
     public int compareTo(Customer other) {
-        return Integer.compare(this.importance, other.importance);
+        return Integer.compare(this.years, other.years);
     }
 }
